1e1b33101cb9c22950ed2f0aec8f3926f60f80d2
Nabila Nowshin <nabilanowshin327@gmail.com>
2025-07-14 16:48:55 +0600

    add Cart component with item list, totals, and clear functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './app.css';
+import Cart from './Components/Cart';
 import PizzaList from './Components/PizzaList';
 function App() {
   return (
     <div className="max-w-7xl mx-auto">
       <h1 className="text-3xl font-bold text-center my-6">SliceMaster Pizza Shop</h1>
       <PizzaList />
+      <Cart></Cart>
     </div>
   )
 }
---
+++ src/Components/Cart.tsx
@@ -0,0 +1,49 @@
+
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, removeFromCart } from "../store/cartSlice";
+
+const Cart = () => {
+
+    const {items,totalQuantity, totalPrice}=useSelector((state)=> state.cart);
+
+    const dispatch=useDispatch();
+    return (
+<div className="max-w-xl mx-auto mt-8 p-4 border rounded shadow">
+      <h2 className="text-2xl font-semibold mb-4">🛒 Cart</h2>
+
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className="flex justify-between items-center border-b py-2"
+        >
+          <div>
+            <p className="font-medium">{item.name}</p>
+            <p className="text-sm text-gray-600">
+              ${item.price} x {item.quantity}
+            </p>
+          </div>
+          <button
+            onClick={() => dispatch(removeFromCart(item.id))}
+            className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+
+      <div className="mt-4 font-semibold">
+        Total Items: {totalQuantity} <br />
+        Total Price: ${totalPrice.toFixed(2)}
+      </div>
+
+      <button
+        onClick={() => dispatch(clearCart())}
+        className="mt-4 bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800"
+      >
+        Clear Cart
+      </button>
+    </div>
+  );
+};
+
+export default Cart;
