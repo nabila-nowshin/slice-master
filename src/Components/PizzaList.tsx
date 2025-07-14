84f21075198d4b807adc1dc553e41017244e5410
Nabila Nowshin <nabilanowshin327@gmail.com>
2025-07-14 16:39:04 +0600

    add Redux cart slice with add, remove, and clear cart logic

--- src/Components/PizzaList.tsx
+++ src/Components/PizzaList.tsx
@@ -1,7 +1,10 @@
+import { useDispatch } from "react-redux";
 import { pizzas, type Pizza } from "../data/pizzas";
+import { addToCart } from "../store/cartSlice";
 
 
 const PizzaList = () => {
+    const dispatch=useDispatch();
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-4">
         {pizzas.map((pizza: Pizza) => (
@@ -13,7 +16,7 @@ const PizzaList = () => {
           <p className="text-gray-600">${pizza.price.toFixed(2)}</p>
           <button
             className="mt-2 bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600"
-            onClick={() => alert(`Add ${pizza.name} to cart (coming soon!)`)}
+            onClick={() => dispatch(addToCart(pizza))}
           >
             Add to Cart
           </button>
